930402496db5f5c5f586d8d7dd8f9e232cdf45f8
Sơn <lazimun13@gmail.com>
2023-06-29 14:38:32 +0700

    add data customize

--- Assets/_Game/Minh An/Scripts/Datas/DataCustomizeController.cs
+++ Assets/_Game/Minh An/Scripts/Datas/DataCustomizeController.cs
@@ -6,47 +6,25 @@ using System.IO;
 public class DataCustomizeController : MonoBehaviour
 {
     [SerializeField] private List<InfoSkinPlayer> infoSkinPlayers_Head;
-    private DataCustomize_Head dataCustomize_Head = new DataCustomize_Head();
-    private bool isInItData = false;
+    private DataCustomize_Head dataCustomize_Head;
+
     private void Awake()
     {
         InItData();
     }
-    private void Start()
-    {
-        EnventManager.AddListener(EventName.ClearData.ToString(), () =>
-        {
-            dataCustomize_Head.ClearData();
-        });
-
-    }
     private void InItData()
     {
         dataCustomize_Head.InItData();
-        isInItData = true;
-    }
-    public DataCustomize_Head GetDataCustomize_Head()
-    {
-        if (!isInItData)
-        {
-            InItData();
-        }
-        return dataCustomize_Head;
-    }
-    public List<InfoSkinPlayer> GetInfoSkinPlayers()
-    {
-        return infoSkinPlayers_Head;
     }
 }
 [System.Serializable]
 public class DataCustomize_Head : DataBase
 {
-    public Data_Head data_Head = new Data_Head();
+    public Data_Head data_Head;
     public void InItData()
     {
         SetFileName(nameof(DataCustomize_Head));
     }
-  
     public override void SaveData()
     {
         base.SaveData();
@@ -64,12 +42,6 @@ public class DataCustomize_Head : DataBase
     {
         data_Head.ResetData();
     }
-    public void ClearData()
-    {
-        ResetData();
-        SaveData();
-        LoadData();
-    }
     public Data_Head GetData_Head()
     {
         LoadData();
@@ -80,52 +52,16 @@ public class DataCustomize_Head : DataBase
         GetData_Head().SetID(value);
         SaveData();
         LoadData();
-        EnventManager.TriggerEvent(EventName.NewID_Customize.ToString());
     }
     public int GetID()
     {
         return GetData_Head().GetID();
     }
-    public List<int> GetID_Onboughts()
-    {
-        return GetData_Head().GetID_Onboughts();
-    }
-    public void AddID_Onboughts(int ID)
-    {
-        ID--;
-        GetData_Head().AddID_Onboughts(ID);
-        SaveData();
-        LoadData();
-    }
-    public void SetValueWatchVideo(int ID, int value)
-    {
-        GetData_Head().GetInfoID_WtachVideo(ID).SetValueWatchVideo(value);
-        SaveData();
-        LoadData();
-    }
-    public int GetValueWatchVideo(int ID)
-    {
-        LoadData();
-        return GetData_Head().GetInfoID_WtachVideo(ID).GetValueWatchVideo();
-    }
-    public void AddValueWatchVideo(int ID, int value)
-    {
-        GetData_Head().GetInfoID_WtachVideo(ID).AddValueWatchVideo(value);
-        SaveData();
-        LoadData();
-    }
-    public InfoSkinPlayer GetInfoSkinPlayer_Current()
-    {
-        LoadData();
-        return GetData_Head().GetInfoSkinPlayer_Current();
-    }
 }
-[System.Serializable]
+[System.Serializable] 
 public class Data_Head
 {
     public int ID;
-    public List<int> ID_OnBought;
-    public List<InfoID_WtachVideo> infoID_WtachVideos = new List<InfoID_WtachVideo>();
     public void SetID(int value)
     {
         ID = value;
@@ -134,68 +70,8 @@ public class Data_Head
     {
         return ID;
     }
-    public void AddID_Onboughts(int ID)
-    {
-        if (!ID_OnBought.Contains(ID))
-        {
-            ID_OnBought.Add(ID);
-        }
-    }
-    public List<int> GetID_Onboughts()
-    {
-        return ID_OnBought;
-    }
     public void ResetData()
     {
-      //  Debug.Log("Reset");
-        ID = 1;
-        ID_OnBought = new List<int>();
-        foreach (InfoID_WtachVideo infoID_WtachVideo in infoID_WtachVideos)
-        {
-            infoID_WtachVideo.ResetData();
-        }
-    }
-    public InfoID_WtachVideo GetInfoID_WtachVideo(int ID)
-    {
-        foreach (InfoID_WtachVideo infoID_WtachVideo in infoID_WtachVideos)
-        {
-            if (infoID_WtachVideo.ID == ID)
-            {
-                return infoID_WtachVideo;
-            }
-        }
-        InfoID_WtachVideo infoID_WtachVideo_ = new InfoID_WtachVideo();
-        infoID_WtachVideo_.ID = ID;
-        infoID_WtachVideo_.ResetData();
-        infoID_WtachVideos.Add(infoID_WtachVideo_);
-        return infoID_WtachVideo_;
-    }
-    public InfoSkinPlayer GetInfoSkinPlayer_Current()
-    {
-        InfoSkinPlayer infoSkinPlayer = (InfoSkinPlayer)Resources.Load("Skin\\Head\\Skin " + (ID), typeof(InfoSkinPlayer));
-        return infoSkinPlayer;
-    }
-}
-[System.Serializable]
-public class InfoID_WtachVideo
-{
-    public int ID;
-    public int valueWatchVideo;
-
-    public void SetValueWatchVideo(int value)
-    {
-        valueWatchVideo = value;
-    }
-    public int GetValueWatchVideo()
-    {
-        return valueWatchVideo;
-    }
-    public void AddValueWatchVideo(int value)
-    {
-        valueWatchVideo += value;
-    }
-    public void ResetData()
-    {
-        valueWatchVideo = 0;
+        ID = 0;
     }
 }
--- Assets/_Game/Minh An/Scripts/Datas/InfoSkinPlayer.cs
+++ Assets/_Game/Minh An/Scripts/Datas/InfoSkinPlayer.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "InfoSkinPlayer")]
 public class InfoSkinPlayer : ScriptableObject
 {
-    public int ID;
-    public Object modelSkin;
-    public Sprite Icon;
+    public GameObject modelSkin;
+
     public InfoBuy infoBuy;
 }
